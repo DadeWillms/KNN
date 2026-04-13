a265fa26b3406c035388968930ee09bb2f68dd7a
DadeWillms <126916738+DadeWillms@users.noreply.github.com>
2026-04-13 08:30:23 -0500

    Add files via upload

--- KNN.py
+++ KNN.py
@@ -41,6 +41,7 @@ print("Accuracy:", accuracy_score(y_test, y_pred))
 print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
 print("Classification Report:\n", classification_report(y_test, y_pred))
 
+#----------  Best K values #----------
 k_values = range(1, 21)
 accuracies = []
 
@@ -60,3 +61,44 @@ plt.xticks(k_values)
 plt.grid(True)
 plt.show()
 
+import seaborn as sns
+
+#---------- confustion matrix #----------
+cm = confusion_matrix(y_test, y_pred)
+
+plt.figure(figsize=(6,4))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.title("Confusion Matrix Heatmap")
+plt.show()
+
+#----------  Decision Boundary Graph #----------
+X_vis = X[["radius_worst", "concave points_worst"]]
+X_vis_scaled = scaler.fit_transform(X_vis)
+
+X_train_v, X_test_v, y_train_v, y_test_v = train_test_split(
+    X_vis_scaled, y, test_size=0.2, random_state=42
+)
+
+model = KNeighborsClassifier(n_neighbors=5)
+model.fit(X_train_v, y_train_v)
+
+h = 0.02
+x_min, x_max = X_vis_scaled[:, 0].min() - 1, X_vis_scaled[:, 0].max() + 1
+y_min, y_max = X_vis_scaled[:, 1].min() - 1, X_vis_scaled[:, 1].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                     np.arange(y_min, y_max, h))
+
+Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+plt.figure(figsize=(8,6))
+plt.contourf(xx, yy, Z, alpha=0.3)
+
+plt.scatter(X_vis_scaled[:, 0], X_vis_scaled[:, 1], c=y, edgecolor='k')
+plt.title("KNN Decision Boundary")
+plt.xlabel("radius_worst")
+plt.ylabel("concave points_worst")
+plt.show()
